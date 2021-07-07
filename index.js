32be47b80266b3f1e31dbea4e27226b60f7f1eb9
sankave00 <69105787+sankave00@users.noreply.github.com>
2021-07-08 00:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ function soundfunc(key)
 {
     switch(key)
     {
-        case 'w': var audio = new Audio("sounds/tom-1.mp3");
+        case 'w': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/tom-1.mp3?raw=true");
                   audio.play();
                   break;
-        case 'a': var audio = new Audio("sounds/tom-2.mp3");
+        case 'a': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/tom-2.mp3?raw=true");
                   audio.play();
                   break;
-        case 's': var audio = new Audio("sounds/tom-3.mp3");
+        case 's': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/tom-3.mp3?raw=true");
                   audio.play();
                   break;
-        case 'd': var audio = new Audio("sounds/tom-4.mp3");
+        case 'd': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/tom-4.mp3?raw=true");
                   audio.play();
                   break;
-        case 'j': var audio = new Audio("sounds/kick-bass.mp3");
+        case 'j': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/kick-bass.mp3?raw=true");
                   audio.play();
                   break;
-        case 'k': var audio = new Audio("sounds/snare.mp3");
+        case 'k': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/snare.mp3?raw=true"");
                   audio.play();
                   break;
-        case 'l': var audio = new Audio("sounds/crash.mp3");
+        case 'l': var audio = new Audio("https://github.com/sankave00/Drum-kit/blob/main/crash.mp3?raw=true"");
                   audio.play();
                   break;
     }
